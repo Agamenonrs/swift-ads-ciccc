8d5ffa19ec79d213f4af02b96043c108d0ff165d
Agamenon Rocha Dos Santos <aga@Agamenons-MacBook-Pro.local>
2020-07-14 11:03:36 -0700

    Add SubstringSearch

---
+++ SubstringSearch/SubstringSearch/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  SubstringSearch
+//
+//  Created by Agamenon Rocha Dos Santos on 12/07/20.
+//  Copyright © 2020 Agamenon. All rights reserved.
+//
+
+import Foundation
+
+//print(calcPrefixLen(pattern: "abcdeabab"))
+
+let pattern = "abcdeabab"
+let text = "wewewabcdeababrereabcdeabababcdeabab"
+print(searchKMP(text: text, pattern: pattern))
+//print(pattern.hashValue)
+print(searchKMP(text: text, pattern: pattern))
