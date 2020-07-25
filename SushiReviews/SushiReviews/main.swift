94fdd42959af3ebd3aa6e0ad929ad07e1f38aed9
Agamenon Rocha Dos Santos <aga@Agamenons-MacBook-Pro.local>
2020-07-25 00:56:40 -0700

    Add SushiReview

---
+++ SushiReviews/SushiReviews/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  SushiReviews
+//
+//  Created by Agamenon Rocha Dos Santos on 23/07/20.
+//  Copyright © 2020 Agamenon. All rights reserved.
+//
+
+import Foundation
+
+shortPath()
