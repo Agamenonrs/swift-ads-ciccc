704a91fbe7fd7267c83fff986fa0164c973c2e5c
Agamenon Rocha Dos Santos <aga@Agamenons-MacBook-Pro.local>
2020-07-29 06:44:04 -0700

    Add DAGTopologicalSort Assignment

---
+++ DAGTopologicalSort/DAGTopologicalSort/CourseSchedule2.swift
@@ -0,0 +1,44 @@
+//
+//  CourseSchedule2.swift
+//  DAGTopologicalSort
+//
+//  Created by Agamenon Rocha Dos Santos on 28/07/20.
+//  Copyright © 2020 Agamenon. All rights reserved.
+//
+
+import Foundation
+
+func findOrder(_ numCourses: Int, _ prerequisites: [[Int]]) -> [Int] {
+    
+    var result: [Int] = []
+    var indegree = [Int](repeating: 0, count: numCourses)
+    var adj = [[Int]](repeating: [Int](), count: numCourses)
+
+    for p in prerequisites {
+        let u = p[0]
+        let v = p[1]
+        adj[v].append(u)
+        indegree[u] += 1
+    }
+    
+    let q = Queue<Int>()
+    for i in 0..<numCourses {
+        if indegree[i] == 0 {
+            q.enqueue(item: i)
+        }
+    }
+    
+    // BSF
+    while !q.isEmpty(){
+        let u = q.dequeue()!
+        result.append(u)
+        for v in adj[u] {
+            indegree[v] -= 1
+            if indegree[v] == 0 {
+                q.enqueue(item: v)
+            }
+        }
+    }
+    return result
+}
+
---
+++ DAGTopologicalSort/DAGTopologicalSort/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  DAGTopologicalSort
+//
+//  Created by Agamenon Rocha Dos Santos on 28/07/20.
+//  Copyright © 2020 Agamenon. All rights reserved.
+//
+
+import Foundation
+
+//print(findOrder(4, [[1,0],[2,0],[3,1],[3,2]]))
+//print(isPossibleFinishAllCourses(2,[[1,0]]))
+print(isPossibleFinishAllCourses(2,[[1,0],[0,1]]))
+
