4a2b410096fa439c67ba76b255a329cc73b001ad
Agamenon Rocha Dos Santos <aga@Agamenons-MacBook-Pro.local>
2020-07-30 12:30:01 -0700

    Improve GasStationAlgorithm

--- GreedyAlgorithmAssignment/GreedyAlgorithmAssignment/GasStation.swift
+++ GreedyAlgorithmAssignment/GreedyAlgorithmAssignment/GasStation.swift
@@ -9,18 +9,13 @@
 import Foundation
 
 func canCompleteCircuit(_ gas: [Int], _ cost: [Int]) -> Int {
-    var diffs = [Int](repeating: 0, count: gas.count)
-    
-    for i in 0..<gas.count {
-        let result = gas[i] - cost[i]
-        diffs[i] = result
-    }
+   
     var index = -1
     for i in 0..<gas.count{
         
         var sum = 0
         for j in i..<gas.count * 2 - 1 {
-            sum += diffs[j %  gas.count]
+            sum += gas[j %  gas.count] - cost[j %  gas.count]
             if sum < 0 {
                 break
             }
@@ -29,8 +24,6 @@ func canCompleteCircuit(_ gas: [Int], _ cost: [Int]) -> Int {
             index = i
             break
         }
-        print(diffs)
     }
-    //print(totalCost)
     return index;
 }
